47ccd978c403de6bdb4c19fbc44f0b45649778c3
OlwethuMsh <omshumpela@gmail.com>
2022-12-08 03:36:46 -0800

    Print f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  * Return: Always 0 (Success)
@@ -7,3 +8,4 @@ int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
+}
