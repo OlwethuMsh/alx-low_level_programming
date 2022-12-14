c514f14766d2724353f0ec209ea28f347ce999c4
OlwethuMsh <omshumpela@gmail.com>
2022-12-13 17:35:17 -0800

    task 02

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * alphabet - Entry point
+ *
+ * print_alphabet_x10_ - print a - z ten times
+ *
+ */
+
+void print_alphabet_x10_(void)
+{
+	int i;
+	char j;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+
+	remove(0);
+}
