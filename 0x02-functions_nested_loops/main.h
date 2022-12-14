874f849c04a83906cc946571719c898331c1dba1
OlwethuMsh <omshumpela@gmail.com>
2022-12-13 16:43:20 -0800

    Putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+/**
+ * functions prototype
+ */
+
+int _putchar(char c);
+void print_akphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+int add(int, int);
+void jack_bauer(void);
+void times_table(void);
+void print_to_98(int n);
+void print_times_table(int n);
+
+#endif
